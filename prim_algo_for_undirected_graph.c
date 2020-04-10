d56ab639384586f047b416e64f2de5a679488488
Supriyo Adhikary <61983291+crs1903@users.noreply.github.com>
2020-04-10 13:44:30 +0530

    Update prim_algo_for_undirected_graph.c

--- prim_algo_for_undirected_graph.c
+++ prim_algo_for_undirected_graph.c
@@ -17,7 +17,7 @@ typedef struct key node;
 vertex *V;
 node **g,**t;
 int **adj,*H,H_size=0;
-double **W;
+double **W,**Wt;
 
 void relax(int i,int j);
 void prim_algo(int s,int n);
@@ -25,7 +25,6 @@ void minheap();
 int extractmin(void);
 void initialize(int n);
 void build_graph(int n);
-int check(int n);
 void insert(int ,int);
 void put_edge(int,int);
 
@@ -38,14 +37,12 @@ int main()
 	V = (vertex*)malloc(n*sizeof(vertex));
 	H = (int*)malloc(n*sizeof(int));
 	g = (node**)malloc(n*sizeof(node*));
-	t = (node**)malloc(n*sizeof(node*));
-	adj = (int **)malloc(n*sizeof(int));
+	adj = (int **)malloc(n*sizeof(int*));
 	W = (double**)malloc(n*sizeof(double*));
-	
 	for(i=0;i<n;i++)
 	{
 		adj[i] = (int*)malloc(n*sizeof(int)); 
-		W[i] = (double*)calloc(n,sizeof(double));
+		W[i] = (double*)malloc(n*sizeof(double));
 	}
 	printf("Enter the adjacent matrix:\n");
 	for(i=0;i<n;i++)
@@ -55,14 +52,18 @@ int main()
 			if(i == j)
 				adj[i][j] = 0;
 		}
-	
-	if(check(n) == 0)
-	{
-		printf("\nGraph is not an undirected graph\n");
-		exit(1);
-	}
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+		{
+			if(adj[i][j] != adj[j][i])
+			{
+				printf("The graph must be an undirected graph\n\n");
+				exit(1);
+			}
+		}
 	printf("\n\n");
 	for(i=0;i<n;i++)
+	{
 		for(j=0;j<i;j++)
 		{
 			if(adj[i][j] == 1)
@@ -70,13 +71,11 @@ int main()
 				printf("weight of edge (%2d,%2d) : ",i+1,j+1);
 				scanf("%lf",&W[i][j]);
 				W[j][i] = W[i][j];
-				if(W[i][j] == 0)
-				{
-					printf("\n\n*****Enter positive weight*****\n\n");
-					j--;
-				}
 			}
+			else
+				W[i][j] = W[j][i] = INFINITY;
 		}
+	}
 	
 	initialize(n);
 	build_graph(n);
@@ -97,7 +96,12 @@ int main()
 		}
 		printf("\n");
 	}
-	
+	double cost =0;
+	for(i=0;i<n;i++)
+		for(j=0;j<i;j++)
+			if(Wt[i][j] != INFINITY)
+				cost+=Wt[i][j];
+	printf("\n\nThe cost is : %.3lf\n\n",cost);
 
 }
 void minheap()
@@ -106,7 +110,7 @@ void minheap()
 	n = H_size;
 	for(p=1;p<n;p++)
 	{	
-		int k=p;
+		k=p;
 		do
 		{	
 			j=(k-1)/2;
@@ -137,7 +141,7 @@ void put_edge(int i,int j)
 {
 	node 	*p = (node *)malloc(sizeof(node)),
 		*q = (node *)malloc(sizeof(node));
-	
+	Wt[i][j] = Wt[j][i] = W[i][j];
 	p->v = i;
 	p->next = t[j];
 	t[j] = p;
@@ -148,10 +152,18 @@ void put_edge(int i,int j)
 }
 void prim_algo(int s,int n)
 {
-	int k;	
+	t = (node**)malloc(n*sizeof(node*));
+	Wt = (double**)malloc(n*sizeof(double*));
+	int k,i,j;	
 	H[H_size++] = s;
 	V[s].visit = 1;
-	
+	for(i=0;i<n;i++)
+	{	
+		t[i] = NULL;
+		Wt[i] = (double*)calloc(n,sizeof(double));
+		for(j=0;j<n;j++)
+			Wt[i][j] = INFINITY;
+	}
 	while(H_size != 0)
 	{
 		k = extractmin();
@@ -174,15 +186,6 @@ void prim_algo(int s,int n)
 		}
 		V[k].visit = 2;	;
 	}
-}
-int check(int n)
-{
-	int i,j;
-	for(i=0;i<n;i++)
-		for(j=0;j<i;j++)	
-			if(adj[i][j] != adj[j][i])
-				return 0;
-	return 1;
 }
 void initialize(int n)
 {
@@ -220,4 +223,3 @@ void build_graph(int n)
 		}
 	}
 }
-
